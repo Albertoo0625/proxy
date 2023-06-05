c848b7f068cd50279a006d4cdbf6c3c4acfa15cc
Albert Too Kipchirchir <tooalbert79@gmail.com>
2023-06-05 12:23:21 +0300

    test1

--- controllers/streamController.js
+++ controllers/streamController.js
@@ -26,17 +26,6 @@ const handleRequest = async (req, res) => {
       });
     });
 
-    // Listen on a dynamic port
-    server.on('error', (error) => {
-      if (error.code === 'EADDRINUSE') {
-        console.error('Port already in use, retrying with a different port...');
-        server.close();
-        server.listen(0);
-      } else {
-        console.error('Server encountered an error:', error);
-      }
-    });
-
     server.listen(0, () => {
       assignedPort = server.address().port;
       console.log(`Server is running on port ${assignedPort}`);
@@ -46,12 +35,12 @@ const handleRequest = async (req, res) => {
     // Create a separate HTTP server to send the port number as the response
     const responseServer = createServer((req, serverResponse) => {
       serverResponse.setHeader('Content-Type', 'text/plain');
-      serverResponse.end(assignedPort.toString()); // Send the port number as the response
+      serverResponse.end(JSON.stringify({ port: assignedPort })); // Send the port number as the response
     });
 
-    // Listen on a fixed port for sending the response
-    responseServer.listen(3000, () => {
-      console.log('Response server is running on port 3000');
+    responseServer.listen(0, () => {
+      const responseServerPort = responseServer.address().port;
+      console.log(`Response server is running on port ${responseServerPort}`);
     });
   } catch (error) {
     console.error('Error handling request:', error);
